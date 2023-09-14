78ef052d825ccd45940b4bd33d858b49cb82d8c8
Ali <alirezamonfared813@gmail.com>
2023-09-13 18:00:17 -0700

    Initial Commit

---
+++ lab5/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lab5
+//
+//  Created by Alireza Karimi on 2023-09-13.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
